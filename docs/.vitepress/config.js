8b2d667df246b1dee32dbfc04f9104022ce0d5b8
Adina FXV <adinafxv@protonmail.com>
2022-12-28 16:51:28 +0100

    updateL

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -14,15 +14,23 @@ export default {
       description: 'Freelance UX Designer'
     }
   },
+
+  head: [
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/fav/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/fav/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/fav/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "/fav/site.webmanifest"}],
+    ['link', { rel: "mask-icon", href: "/fav/safari-pinned-tab.svg", color: "#e6c341"}],
+    ['link', { rel: "shortcut icon", href: "/fav/favicon.ico"}],
+    ['meta', { name: "msapplication-TileColor", content: "#3a0839"}],
+    ['meta', { name: "msapplication-config", content: "/fav/browserconfig.xml"}],
+    ['meta', { name: "theme-color", content: "#e6c341"}],
+  ],
+  // ...
   
   
   themeConfig: {
-    siteTitle: false,
-    logo: { 
-      light: '/logo.svg',
-      dark: '/logoDark.svg',
-      alt: '{fxv}'
-    },
+    siteTitle: '{fxv}',
     
     /* locales: {
       '/': {
