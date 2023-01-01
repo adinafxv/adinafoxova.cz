7fb9fe19ec1685e3b95c1f78e43af4b46b942297
Adina FXV <adinafxv@protonmail.com>
2023-01-01 21:26:55 +0100

    fix favicon PT2

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -16,14 +16,14 @@ export default {
   },
 
   head: [
-    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/public/fav/apple-touch-icon.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/public/fav/public/favicon-32x32.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/public/fav/public/favicon-16x16.png"}],
-    ['link', { rel: "manifest", href: "/public/fav/site.webmanifest"}],
-    ['link', { rel: "mask-icon", href: "/public/fav/safari-pinned-tab.svg", color: "#e6c341"}],
-    ['link', { rel: "shortcut icon", href: "/public/fav/favicon.ico"}],
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/fav/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/fav/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/fav/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "/fav/site.webmanifest"}],
+    ['link', { rel: "mask-icon", href: "/fav/safari-pinned-tab.svg", color: "#e6c341"}],
+    ['link', { rel: "shortcut icon", href: "/fav/favicon.ico"}],
     ['meta', { name: "msapplication-TileColor", content: "#3a0839"}],
-    ['meta', { name: "msapplication-config", content: "/public/fav/browserconfig.xml"}],
+    ['meta', { name: "msapplication-config", content: "/fav/browserconfig.xml"}],
     ['meta', { name: "theme-color", content: "#e6c341"}],
   ],
   // ...
